9cffb5b8f6fa183fc4a76d349d1371d2508274c6
brunonico <brunonicolas10@hotmail.com>
2021-08-23 16:48:08 -0300

    change getJSONData for a normal fetch

--- js/login.js
+++ js/login.js
@@ -1,21 +1,4 @@
 
 document.addEventListener("DOMContentLoaded", function (e) {
-    document.getElementById("enterBtn").addEventListener("click", function (e) {
-
-        let inputUser = document.getElementById("username");
-        let inputPass = document.getElementById("password");
-        let fulFilled = true;
-
-
-        if (inputUser.value === '' || inputPass.value === '') {
-            fulFilled = false;
-            alert("Debes ingresar tus datos para continuar.");
-        }
-
-        if (fulFilled) {
-            window.location = 'home.html';
-        }
-
-    });
 
 });
--- js/products.js
+++ js/products.js
@@ -2,46 +2,41 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCTS_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            productsArray = resultObj.data;
-            showProducts(productsArray);
-            
-        }
-    })
-}); 
 
-//obtengo el json y lo transformo en un array
+    fetch(PRODUCTS_URL)
+        .then(response => response.json())  // se me guardan los datos en el response y ejecuto response.json, transformando la respuesta a formato json
+        .then(data => { //data es el array 
 
-function showProducts(productsArray) {
+            let content = ""; //string que formará el contenido de la página
+            for (let i = 0; i < data.length; i++) {
+                let products = data[i]; /* iteramos en el array para sacar los datos que necesitamos y los almacenamos en la variable products. Luego, a partir de ella accederemos a los atributos
+                que colocaremos en el html */
 
-    let content = "";
-    for (let i = 0; i < productsArray.length; i++) {
-        let products = productsArray[i];
-        //itero en ese array para agregarle su contenido a los parámetros del html
-        content +=
-            `<div class="list-group-item list-group-item-action">
-            <div class="row">
-            <div class="col-3">
-                <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
-            </div>
-            <div class="col">
-                <div class="d-flex w-100 justify-content-between">
-                    <h4 class="mb-1">`+ products.name +`</h4>
-                    <strong class="text-muted">` + products.cost + " " + products.currency + `</strong>
+                content +=
+                    `<div class="list-group-item list-group-item-action">
+                <div class="row">
+                <div class="col-3">
+                    <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
                 </div>
-                <p class="mb-1">` + products.description + `</p>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ products.name + `</h4>
+                        <strong class="text-muted">` + products.cost + " " + products.currency + `</strong>
+                    </div>
+                    <p class="mb-1">` + products.description + `</p>
+                </div>
             </div>
-        </div>
-        </div>
-        <br>
-            `
+            </div>
+            <br>
+                `
 
 
-    }
+            }
 
-    document.getElementById("prod-list-container").innerHTML = content;
+            document.getElementById("prod-list-container").innerHTML = content; //le asignamos el valor content al id prod-list-container a través de DOM
 
-    //utilizando dom, inserto el contenido en la id 
-}
+        })
+})
+
+
 
