dc4647a2eb9615d8b751b849433a0dbea1820034
brunonico <brunonicolas10@hotmail.com>
2021-08-26 21:01:12 -0300

    sessionStorage with inputName, appear in navBar when sign in

--- js/login.js
+++ js/login.js
@@ -1,4 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    document.getElementById("enterBtn").onclick = function() { 
+        let name = document.getElementById("inputName").value;
+        sessionStorage.setItem ("inputName", name)
+    }
 });
