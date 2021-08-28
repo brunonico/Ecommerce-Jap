26bc1d85bf6c96c2ec3cf4fade993c0a2c9bf52b
brunonico <brunonicolas10@hotmail.com>
2021-08-27 21:58:13 -0300

    filter and delete button added to products

--- js/products.js
+++ js/products.js
@@ -1,10 +1,19 @@
+let minPrice = undefined;
+let maxPrice = undefined;
+var productsArray = [];
+
+
 function showProducts(array) {
+
     let content = "";
     for (let i = 0; i < array.length; i++) {
         let products = array[i];
 
-        content +=
-            `<div class="list-group-item list-group-item-action">
+
+        if (((minPrice == undefined) || (minPrice != undefined && parseInt(products.cost) >= minPrice))
+            && ((maxPrice == undefined) || (maxPrice != undefined && parseInt(products.cost) <= maxPrice))) {
+            content +=
+                `<div class="list-group-item list-group-item-action">
                 <div class="row">
                 <div class="col-3">
                     <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
@@ -24,17 +33,51 @@ function showProducts(array) {
             <br>
                 `
 
+        }
+    }
+    document.getElementById("prod-list-container").innerHTML = content;
+}
+
+
+
+document.getElementById("filter").addEventListener("click", function () {
+    minPrice = document.getElementById("minimum").value;
+    maxPrice = document.getElementById("maximum").value;
+
+    if ((minPrice != undefined) && (minPrice != "") && (parseInt(minPrice)) >= 0) {
+        minPrice = parseInt(minPrice);
     }
 
-    document.getElementById("prod-list-container").innerHTML = content;
+    else {
+        minPrice = undefined;
+    }
+    if ((maxPrice != undefined) && (maxPrice != "") && (parseInt(maxPrice)) >= 0) {
+        maxPrice = parseInt(maxPrice);
+    }
+    else {
+        maxPrice = undefined;
+    }
+    showProducts(productsArray);
+});
 
-}
+document.getElementById("erase").addEventListener("click", function(){
+    document.getElementById("minimum").value = "";
+    document.getElementById("maximum").value = "";
+
+    minPrice = undefined;
+    maxPrice = undefined;
+
+    showProducts(productsArray);
+});
+
 
 document.addEventListener("DOMContentLoaded", function (e) {
     fetch(PRODUCTS_URL)
         .then(response => response.json())
-        .then(data => {
-            showProducts(data);
+        .then((data) => {
+            productsArray = data;
+
+            showProducts(productsArray)
         }
         )
 });
