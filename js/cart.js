009425663f8e0b2ca5ac42eaf08b73ed69d26faa
brunonico <brunonicolas10@hotmail.com>
2021-10-07 00:52:15 -0300

    first hand on cart

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,16 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
+document.addEventListener("DOMContentLoaded", function (e) {
+    fetch(CART_INFO_URL)
+        .then(res => res.json())
+        .then(info =>
+            showChart(info))
 });
+
+
+function showChart(param) {
+    let articulo =  param.articles[0];
+    
+    $("#cartImg").attr('src',articulo.src);
+}
