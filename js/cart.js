997d5540a92070546dfedcfd1d68e1e4fcd12773
brunonico <brunonicolas10@hotmail.com>
2021-10-11 23:59:24 -0300

    still working and looking for a better chart

--- js/cart.js
+++ js/cart.js
@@ -1,59 +1,65 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
     fetch("https://japdevdep.github.io/ecommerce-api/cart/654.json")
         .then(res => res.json())
-        .then(info =>
-            showChart(info))
+        .then(info => {
+            let cartArray = info.articles;
+            showChart(cartArray)
+        })
 });
 
 
-function showChart(param) {
-    let articulo = param.articles;
+function showChart(cartArray) {
+
     let cartContent = ``;
-    let cost=[];
-    for (let i = 0; i < articulo.length; i++) {
+
+    for (let i = 0; i < cartArray.length; i++) {
+        let articulo = cartArray[i];
 
         cartContent +=
-            `<div class="card">      
-
-                <img class="img-thumbnail h-50 w-50" src="${articulo[i].src}" alt="Card image cap">
+            `<div class="card cartCard">      
+                <img class="img-thumbnail h-50 w-50" src="${articulo.src}" alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title">${articulo[i].name}</h5>
-                    <p class="card-text"><input class="inputCount" type="number" value="${articulo[i].count}"> Cantidad </p>
-                    <div class="cartPrice">${articulo[i].currency == 'UYU' ? '$' : 'u$s'} <span id="thePrice">${articulo[i].unitCost}</span></div>
-                    
+                    <h5 class="card-title">${articulo.name}</h5>
+                    <div class="cartPrice">Precio unitario: ${articulo.currency == 'UYU' ? '$' : 'u$s'} <span class="unitPrice">${articulo.unitCost}</span></div>   
+                    <p class="card-text"><input class="inputCount" id="count${i}" type="number" onchange="subtotalCalc(${articulo.unitCost},${i})" value="${articulo.count}"> Cantidad </p>          
+                    <div id="subtotal${i}" class="total" style="font-weight:bold;"> ${articulo.currency == 'UYU' ? '$' : 'u$s'} ${articulo.unitCost}</div>
                     <button href="#!" class="deleteButton"><i class="fas fa-minus"></i></button>
                 </div>  
             </div>     
 `
-        cost.push(articulo[i].unitCost);
+
     }
+
+
     $("#kardec").append(cartContent);
-    console.log(cost)
+
     $(".deleteButton").click(function () {
         $(this).parents(".card").remove();
     });
+    $(".inputCount").change(function () {
+        var actualInput = $(this).val()
+        $(".thePrice").replaceWith(actualInput);
+    })
+
 }
 
-    // $("#productCostText").html($(".subtotal").text());
-    // $("#shippingText").html($("input:checked").val() + "%");
-    // var shippingCost = ($("input:checked").val() / 100) * parseInt($(".subtotal").text());
-    // $("#totalCostText").html(shippingCost + parseInt($(".subtotal").text()))
 
-    // $("#inputCount").change(function () {
-    //     var actualInput = $("#inputCount").val() * articulo.unitCost;
-    //     let newSubTotal = `<td>${actualInput} ${articulo.currency}</td>`
-    //     document.getElementById("subtotal").innerHTML = newSubTotal;
-    //     document.getElementById("productCostText").innerHTML = newSubTotal;
-    // })
+function conversion(currency, cost) {
+    if (currency == 'USD') {
+        return cost * 45
+    }
+    else {
+        return cost;
+    }
+}
 
-
-    // $("input").on("click", function () {
-    //     $("#shippingText").html($("input:checked").val() + "%");
-    //     $("#totalCostText").html(($("input:checked").val() / 100) * parseInt($("#productCostText").text()) + parseInt($("#productCostText").text()))
-    // });
+$("#shippingText").html($("input:checked").val() + "%");
 
 
+function subtotalCalc (unitCost, i) {
+    let count = parseInt(document.getElementById(`count${i}`).value);
 
+    let subTot = unitCost * count;
+    document.getElementById(`subtotal${i}`).innerHTML = subTot;
+
+}
